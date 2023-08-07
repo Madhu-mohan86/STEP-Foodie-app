81a52b549102a39607ca801e577e6994dfe00ea3
scully86 <124799557+scully86@users.noreply.github.com>
2023-08-07 23:29:43 +0530

    third commit

--- src/Components/Itemslist.jsx
+++ src/Components/Itemslist.jsx
@@ -3,8 +3,9 @@ import Button from './Button'
 
 export default function Itemslist({foodCard,hotelName,itemName,itemPrice,hotelDistance,rating}) {
   return (
-    <div className='flex justify-center mt-10'>
-    <div className='border border-black w-64'>
+    <div className='flex flex-row justify-center mt-10'>
+  
+    <div className='w-64 border border-black'>
       
       {/* Image part */}
 
@@ -12,8 +13,8 @@ export default function Itemslist({foodCard,hotelName,itemName,itemPrice,hotelDi
       
       {/* Text part */}
       <div className='px-1.5 py-2'>
-         <h1 className='font-Inter font-semibold text-slate-900'>{hotelName}</h1>
-         <p className='font-Inter text-base font-medium text-gray-400 line-clamp-1' >{itemName}</p>
+         <h1 className='font-semibold font-Inter text-slate-900'>{hotelName}</h1>
+         <p className='text-base font-medium text-gray-400 font-Inter line-clamp-1' >{itemName}</p>
          <span className='flex justify-between my-2'>
 
          {/* extra details */}
@@ -24,16 +25,16 @@ export default function Itemslist({foodCard,hotelName,itemName,itemPrice,hotelDi
                   <path fillRule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
          </svg>
            
-         <p className='text-red-600 font-Inter font-semibold'>{hotelDistance}km</p>
+         <p className='font-semibold text-red-600 font-Inter'>{hotelDistance}km</p>
          
          </span>
        
-         <p className='text-green-500 font-Inter font-semibold'>&#x20B9;&#160;{itemPrice}&#160;onwards</p>
+         <p className='font-semibold text-green-500 font-Inter'>&#x20B9;&#160;{itemPrice}&#160;onwards</p>
          </span>
          
          <Button content='Show catlog' color='#BD031A'/>
          
-         <p className='text-end font-Inter text-base text-yellow-500 font-semibold mt-3'>{rating}&#160;ratings</p>
+         <p className='mt-3 text-base font-semibold text-yellow-500 text-end font-Inter'>{rating}&#160;ratings</p>
       </div>
       
     </div>
--- src/Pages/Restaurantpage.jsx
+++ src/Pages/Restaurantpage.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import Itemslist from '../Components/Itemslist'
 import foodcard from '../Images/food-card.png' 
 import Navbar from '../Components/Navbar'
-
+import axios from 'axios'
 
 export default function Restaurantpage() {
 
-const menu={
+  const [data,setData]=useState();
+
+  useEffect(() => {
+    async function fetchAPI() {
+      try {
+        const response = await axios.get('https://mocki.io/v1/6aebdb48-272f-48cf-9d28-1d2a73a0ccd4');
+        setData(response.data); // Update state with the fetched data
+      } catch (error) {
+        console.error('Error fetching API:', error);
+      }
+    }
+
+    fetchAPI(); // Call the fetchAPI function to fetch data when component mounts
+  }, []);
+
+
+  const menu = data.length > 0 ? data : [{
     food_card:foodcard,
     hotel_name:'Adayar Anandha Bhavan',
     item_name:'Vegetarian, chinese, north indian , tandoori',
     item_price:'50',
     hotel_distance:'4.3',
     ratings:'450'
-}
-
+},
+{
+  food_card:foodcard,
+  hotel_name:'Adayar Anandha Bhavan',
+  item_name:'Vegetarian, chinese, north indian , tandoori',
+  item_price:'50',
+  hotel_distance:'4.3',
+  ratings:'450'
+},
+]
   return (
     <>
     <Navbar userName='Ashok madhavan' />
-    <Itemslist foodCard={menu.food_card} hotelName={menu.hotel_name} itemName={menu.item_name} itemPrice={menu.item_price} hotelDistance={menu.hotel_distance} rating={menu.ratings} />
+   {menu.map((menu)=>{
+    return(<Itemslist foodCard={menu.food_card} hotelName={menu.hotel_name} itemName={menu.item_name} itemPrice={menu.item_price} hotelDistance={menu.hotel_distance} rating={menu.ratings} />
+    )})}
     </>
-  )
+  ) 
 }
